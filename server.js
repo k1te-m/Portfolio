9bda12f37b8f0fac07ef5c00ccd217985cac3a2a
k1te <kmiller343@gmail.com>
2020-11-05 16:22:51 -0600

    setting up server

---
+++ server.js
@@ -0,0 +1,23 @@
+// Dependencies
+const express = require("express");
+
+
+// Set up Express App
+const app = express();
+const router = express.Router();
+
+router.use(express.static("public"));
+let PORT = process.env.PORT || 8080;
+
+const db = require("./models");
+
+// Express Parsing Set-up
+router.use(express.urlencoded({ extended: true }));
+router.use(express.json());
+
+// Handlebars Set-up
+const exphbs = require("express-handlebars");
+
+router.engine("handlebars", exphbs({defaultLayout: "main"}))
+router.set("view engine", "handlebars");
+

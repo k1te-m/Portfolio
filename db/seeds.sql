5549598c20547e55ea7dcc9a2e41de6b967e5754
k1te <kmiller343@gmail.com>
2020-11-05 17:51:29 -0600

    updated seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,9 @@
-INSERT INTO Repos (name, github, imageURL, createdAt, updatedAt) values ("Burger-Logger", "k1te-m", "./images/burger-logger", "2020/11/05", "2020/11/05");
-INSERT INTO Repos (name, github, imageURL, createdAt, updatedAt) values ("Project-1-Restaurant-Roulette", "k1te-m", "./images/rr-1", "2020/11/05", "2020/11/05");
-INSERT INTO Repos (name, github, imageURL, createdAt, updatedAt) values ("courseWork_06_weatherDashboard", "k1te-m", "./images/weatherdemo", "2020/11/05", "2020/11/05");
-INSERT INTO Repos (name, github, imageURL, createdAt, updatedAt) values ("EcommerceApp", "oroth8", "./images/limit", "2020/11/05", "2020/11/05");
-INSERT INTO Repos (name, github, imageURL, createdAt, updatedAt) values ("courseWork_05_dayPlanner", "k1te-m", "./images/planner", "2020/11/05", "2020/11/05");
-INSERT INTO Repos (name, github, imageURL, createdAt, updatedAt) values ("Team-Profile-Generator", "k1te-m", "./images/teamgenerator", "2020/11/05", "2020/11/05");
-INSERT INTO Repos (name, github, imageURL, createdAt, updatedAt) values ("Note-Taker", "k1te-m", "./images/notetaker", "2020/11/05", "2020/11/05");
-INSERT INTO Repos (name, github, imageURL, createdAt, updatedAt) values ("Employee-Tracker", "k1te-m", "./images/EMS", "2020/11/05", "2020/11/05");
+INSERT INTO Repos (name, github, imageURL, createdAt, updatedAt) values ("Burger-Logger", "k1te-m", "./images/burger-logger.png", "2020/11/05", "2020/11/05");
+INSERT INTO Repos (name, github, imageURL, createdAt, updatedAt) values ("Project-1-Restaurant-Roulette", "k1te-m", "./images/rr-1.png", "2020/11/05", "2020/11/05");
+INSERT INTO Repos (name, github, imageURL, createdAt, updatedAt) values ("courseWork_06_weatherDashboard", "k1te-m", "./images/weatherdemo.png", "2020/11/05", "2020/11/05");
+INSERT INTO Repos (name, github, imageURL, createdAt, updatedAt) values ("EcommerceApp", "oroth8", "./images/limit.png", "2020/11/05", "2020/11/05");
+INSERT INTO Repos (name, github, imageURL, createdAt, updatedAt) values ("courseWork_05_dayPlanner", "k1te-m", "./images/planner.png", "2020/11/05", "2020/11/05");
+INSERT INTO Repos (name, github, imageURL, createdAt, updatedAt) values ("Team-Profile-Generator", "k1te-m", "./images/teamgenerator.png", "2020/11/05", "2020/11/05");
+INSERT INTO Repos (name, github, imageURL, createdAt, updatedAt) values ("Note-Taker", "k1te-m", "./images/notetaker.png", "2020/11/05", "2020/11/05");
+INSERT INTO Repos (name, github, imageURL, createdAt, updatedAt) values ("Employee-Tracker", "k1te-m", "./images/EMS.png", "2020/11/05", "2020/11/05");
 

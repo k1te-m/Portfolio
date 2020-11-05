9ec17460039cc7bd37bcdcfd4f0e8b8a03571a25
k1te <kmiller343@gmail.com>
2020-11-05 17:22:35 -0600

    updated repo model

--- models/repo.js
+++ models/repo.js
@@ -15,6 +15,7 @@ module.exports = (sequelize, DataTypes) => {
   };
   Repo.init({
     name: DataTypes.STRING,
+    github: DataTypes.STRING,
     imageURL: DataTypes.STRING
   }, {
     sequelize,

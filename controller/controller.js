edd374e46b9896bce7b066f309d42f405c06a041
k1te <kmiller343@gmail.com>
2020-11-06 15:07:33 -0600

    added post route for creating a new message

--- controller/controller.js
+++ controller/controller.js
@@ -4,24 +4,34 @@ const router = express.Router();
 
 const db = require("../models");
 
-router.get("/", function(req, res) {
-    res.render("index");
-})
+router.get("/", function (req, res) {
+  res.render("index");
+});
 
-router.get("/contact", function(req, res) {
-    res.render("contact");
-})
+router.get("/contact", function (req, res) {
+  res.render("contact");
+});
 
-router.get("/portfolio", function(req, res) {
-    db.Repo.findAll({}).then(results => {
-        let repos = [];
-        for (let index = 0; index < results.length; index++) {
-            repos.push(results[index].dataValues);  
-        }
-        console.log(repos);
-        res.render("portfolio", {layout: "main", repos});
+router.get("/portfolio", function (req, res) {
+  db.Repo.findAll({}).then((results) => {
+    let repos = [];
+    for (let index = 0; index < results.length; index++) {
+      repos.push(results[index].dataValues);
+    }
+    console.log(repos);
+    res.render("portfolio", { layout: "main", repos });
+  });
+});
+
+router.post("/contact/add", function (req, res) {
+  db.Message.create(req.body)
+    .then((results) => {
+      res.json(results);
+      console.log("Message Sent...");
     })
-  
-})
+    .catch(function () {
+      console.log("Error... Please Try Again...");
+    });
+});
 
 module.exports = router;

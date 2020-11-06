2d6ff52a00fb922b512d3439e43b5d9c87e3224c
k1te <kmiller343@gmail.com>
2020-11-06 15:07:14 -0600

    wrote front-end js for contact form

---
+++ public/js/contact.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+ 
+
+  $("#subButton").on("click ", function (event) {
+    event.preventDefault();
+
+    userName = $("#name").val().trim();
+    userEmail = $("#email").val().trim();
+    userMessage = $("textArea").val().trim();
+
+    let newMessage = {};
+    newMessage["name"] = userName;
+    newMessage["email"] = userEmail;
+    newMessage["message"] = userMessage;
+    newMessage["createdAt"] = new Date(Date.now()).toISOString();
+    newMessage["updatedAt"] = new Date(Date.now()).toISOString();
+
+    console.log(newMessage);
+
+    $.post("/contact/add", newMessage).done(function(){
+        alert("Message Sent!");
+        window.location.href= "/contact";
+    });
+  });
+});
